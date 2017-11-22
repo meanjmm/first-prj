8e8f7c0f4e7c26a34e0edb5c3f836a614ae26bba
Jose <meanjmm@gmail.com>
2017-11-22 15:06:20 -0500

    Completed styling the footer

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,33 @@
+const   gulp            = require('gulp'),
+        svgSprite       = require('gulp-svg-sprite'),
+        rename          = require('gulp-rename');
+
+
+const   config = {
+    mode: {
+        css: {
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }
+        }
+    }
+}
+
+
+gulp.task('createSprite', () => {
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+
+gulp.task('copySpriteCSS',['createSprite'], () => {
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteCSS']);
+
